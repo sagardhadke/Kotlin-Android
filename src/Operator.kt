a1296864159fdfc0219635994826f43035219ebc
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-03 19:05:26 +0530

    Relation Operator

--- src/Operator.kt
+++ src/Operator.kt
@@ -1,36 +1,49 @@
 fun main(){
 
-    val a = 5
-    val b = 3
+//    val a = 5
+//    val b = 3
+//
+//    val result = a+b
+//    println("A and B Result is $result")    // 1 method
+//    println("A and B Resukt is ${a+b}")     //2 method
+//
+//    //Arithmetic Operators
+//
+//    println("Arithmetic Operators")
+//    println(a+b)
+//    println("Subtraction of a and b is ${a-b}")
+//    println("a * b = ${a*b}")
+//    println("a / b = ${a/b}")
+//    println("a % b = ${a%b}")
+//
+//    //increment and decrement
+//    var x = 4
+//
+//    println(x++)    //4
+//    println(++x)    //6
+//    println(++x)    //7
+//    println(x)       //7
+//
+//    //Decrement
+//
+//    var y = 9
+//    println(y--) //9
+//    println(--y) //7
+//    println(--y) //6
+//    println(y--) //6
+//    println(y)  //5
+//
+    //Relation Operator
 
-    val result = a+b
-    println("A and B Result is $result")    // 1 method
-    println("A and B Resukt is ${a+b}")     //2 method
+    var num1 = 50
+    var num2 = 60
 
-    //Arithmetic Operators
+    println(num1>num2) //false
+    println(num1<num2) //true
+    println(num1>=num2) //false
+    println(num1<=num2) //true
+    println(num1!=num2) //true
+    println(num1==num2) //false
 
-    println("Arithmetic Operators")
-    println(a+b)
-    println("Subtraction of a and b is ${a-b}")
-    println("a * b = ${a*b}")
-    println("a / b = ${a/b}")
-    println("a % b = ${a%b}")
-
-    //increment and decrement
-    var x = 4
-
-    println(x++)    //4
-    println(++x)    //6
-    println(++x)    //7
-    println(x)       //7
-
-    //Decrement
-
-    var y = 9
-    println(y--) //9
-    println(--y) //7
-    println(--y) //6
-    println(y--) //6
-    println(y)  //5
 
 }
