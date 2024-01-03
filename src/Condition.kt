fc1fd2fbb3d0434ab04f1e1b8f5d28922e30ee8a
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-03 19:54:56 +0530

    if and else

---
+++ src/Condition.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    //if-else statements
+
+    var Bank_Name = "Sagar Bank Of India"
+
+    if (Bank_Name=="Sagar Bank Of India"){
+        println("$1002 Receive on your account")
+    }else{
+        println("Bank Name invalid")
+    }
+}
