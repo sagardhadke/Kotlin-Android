b23be20dfe5bad81627165f9c93b11ad0aef0061
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-03 20:02:42 +0530

    When Statement

--- src/Condition.kt
+++ src/Condition.kt
@@ -1,6 +1,6 @@
 fun main(){
 
-    //if-else statements
+    if-else statements
 
     var Bank_Name = "Sagar Bank Of India"
 
@@ -9,4 +9,19 @@ fun main(){
     }else{
         println("Bank Name invalid")
     }
+
+    //when statement
+
+    val day = "Wednesday"
+    when(day){
+        "Sunday" -> println("Today is Sunday")
+        "Monday" -> println( "Today is Monday")
+        "Tuesday" -> println("Today is Tuesday")
+        "Wednesday" -> println("Today is Wednesday")
+        "Thursday" -> println("Today is Thursday")
+        "Friday" -> println("Today is Friday")
+        "Saturday" -> println("Today is Saturday")
+        else -> println("Invalid Day")
+    }
+
 }
