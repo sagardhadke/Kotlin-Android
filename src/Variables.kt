34a03efc4d337593543302fc878b43418f8ff818
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-03 13:06:42 +0530

    All Variables

---
+++ src/Variables.kt
@@ -0,0 +1,23 @@
+fun main(){
+    // variable in kotlin
+
+    // String => " "
+    // float => 3.4e-038 To 3.4e to 038
+    // Double =>  1.7e-308 To 1.7e 308
+    // Char => 'a-z' & 'A-Z'
+    // Short =>  -32,768 To 32,767
+    // long => -9,223,372,036,854,775,808 To 9,225,036,854,775,807
+    // Byte => -128 To 127
+    // Boolean => True and false
+
+    //String
+    var myName = "Sagar Dhadke"
+    myName = "Hey"
+    println(myName)
+
+    var a = 5
+    var b = 2
+
+    print(a+b)
+
+}
