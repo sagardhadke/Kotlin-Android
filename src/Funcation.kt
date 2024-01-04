d2a73ab0accbeb87c45876c94be1f69034646ade
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-04 11:14:27 +0530

    Functions with small project

--- src/Funcation.kt
+++ src/Funcation.kt
@@ -1,8 +1,24 @@
-fun main(){
-    newYear(year = 2024)
+fun main() {
+//    newYear(year = 2024)
+
+
+//project using function
+
+    val internet = true
+    if (!internet){
+        println("OOps internet connection not found please check your internet!!!")
+    }else{
+        newYear(2024)
+        getData()
+    }
+
 }
 
-fun newYear(year : Int){
+fun getData(){
+    println("Hey Welcome To Android Kotlin Basic to Advance Series")
+    println("I Hope You Love this project:)")
+}
+
+fun newYear(year: Int) {
     println("Happy New Year!!! $year")
 }
-
