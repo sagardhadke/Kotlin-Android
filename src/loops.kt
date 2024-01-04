1c7ab36f845de2477f3aaa6758ce58ba4248271d
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-04 19:29:11 +0530

    All Types of Loops

--- src/loops.kt
+++ src/loops.kt
@@ -9,17 +9,33 @@ fun main(){
     for (i in 1 until 10){
         println(i) // 1 2 3 4 5 6 7 8 9
     }
-
-//    DownTo
-
+//
+////    DownTo
+//
     for (i in 10 downTo 1){
         println(i)  // 10 9 8 7 6 5 4 3 2 1
     }
-
-    //step
-
+//
+//    //step
+//
     for (i in 2 .. 20 step 2){
         println(i)
     }
+
+
+    //while loops
+
+    var num = 0
+    while (num<=10){
+        println(num)
+        num++
+    }
+
+    //do-while
+
+    var d = 9
+    do {
+        println(d)
+    }while (d<10)
 
 }
