0d88e64f40148690fa4266742f6ed99b7fdf3f51
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-05 21:56:54 +0530

    Arithmetic Operators

--- src/Operator.kt
+++ src/Operator.kt
@@ -1,20 +1,20 @@
 fun main(){
 //
-//    val a = 5
-//    val b = 3
-//
-//    val result = a+b
-//    println("A and B Result is $result")    // 1 method
-//    println("A and B Resukt is ${a+b}")     //2 method
+    val a = 5
+    val b = 3
+
+    val result = a+b
+    println("A and B Result is $result")    // 1 method
+    println("A and B Resukt is ${a+b}")     //2 method
 //
 //    //Arithmetic Operators
 //
-//    println("Arithmetic Operators")
-//    println(a+b)
-//    println("Subtraction of a and b is ${a-b}")
-//    println("a * b = ${a*b}")
-//    println("a / b = ${a/b}")
-//    println("a % b = ${a%b}")
+    println("Arithmetic Operators")
+    println(a+b)
+    println("Subtraction of a and b is ${a-b}")
+    println("a * b = ${a*b}")
+    println("a / b = ${a/b}")
+    println("a % b = ${a%b}")
 //
 //    //increment and decrement
     var x = 4
