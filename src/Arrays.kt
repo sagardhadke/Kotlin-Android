66cf3e1667285c5dfe927ba5deacbf55b7052955
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-06 13:21:20 +0530

    Arrays with numbers and loops

--- src/Arrays.kt
+++ src/Arrays.kt
@@ -7,4 +7,13 @@ fun main(){
     println(name[3])
     println(name.size)
 
+    val numbers = arrayOf(2,5,6,8,9,4,25,12,"Sagar","Mango",'S')
+
+    for (i in numbers){
+        if (i is Int){
+            //print only Integer value
+            println(i)
+        }
+    }
+
 }
