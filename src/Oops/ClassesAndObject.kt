96aaaf9a03217c85ecdc3e0d31ebde744c880bc9
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-07 22:35:52 +0530

    Classes and Object in package

--- src/Oops/ClassesAndObject.kt
+++ src/Oops/ClassesAndObject.kt
@@ -1,23 +1,13 @@
-//Class
+package Oops
 class MyClass{
 
-    //Object
     fun firstClass(){
         println("This is Object")
     }
-
 }
 
 fun main(){
-
     //Classes and Object
     val obj = MyClass()
     obj.firstClass()
-
-
-    //Abstraction
-    //Polymorphism
-    //Inheritance
-    //Encapsulation
-
 }
