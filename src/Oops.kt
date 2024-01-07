d20f6760efaa498bc6c69c8c5a519e5d2f15223b
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-07 22:30:51 +0530

    Classes and Object

---
+++ src/Oops.kt
@@ -0,0 +1,23 @@
+//Class
+class MyClass{
+
+    //Object
+    fun firstClass(){
+        println("This is Object")
+    }
+
+}
+
+fun main(){
+
+    //Classes and Object
+    val obj = MyClass()
+    obj.firstClass()
+
+
+    //Abstraction
+    //Polymorphism
+    //Inheritance
+    //Encapsulation
+
+}
