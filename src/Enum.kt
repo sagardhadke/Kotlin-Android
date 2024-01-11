a675fc68c32690ca0c1b84453eda409ecf2b1574
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-11 23:10:25 +0530

    Enum Class with simple Project

--- src/Enum.kt
+++ src/Enum.kt
@@ -2,4 +2,22 @@ fun main(){
 
     //enum means user define
 
+    println("Today is: "+Days.MONDAY)
+
+    for (hol in Days.values()){
+            if (hol.holiday){
+                println("$hol is a holiday")
+            }
+    }
+
+}
+
+enum class Days(val holiday: Boolean = false){
+    SUNDAY (true),
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY(true)
 }
