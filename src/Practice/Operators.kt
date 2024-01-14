e48fc42f2a0e9899dfbf47b59333ba2e615f5c87
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-14 22:09:20 +0530

    Relational Operators

--- src/Practice/Operators.kt
+++ src/Practice/Operators.kt
@@ -11,12 +11,32 @@ fun main(){
     * / division
     * % modules */
 
-    var a = 5
-    var b = 10
-    println(a+b)
-    println(a-b)
-    println(a*b)
-    println(a/b)
-    println(a%b)
+//    var a = 5
+//    var b = 10
+//    println(a+b)
+//    println(a-b)
+//    println(a*b)
+//    println(a/b)
+//    println(a%b)
+
+    //Relational Operators
+
+    /*
+    * < less than
+    * > greater than
+    * <= less than equal to
+    * >= greater than equal to
+    * == equal and equal to
+    * != not equal to*/
+
+    var x = 6
+    var y = 10
+
+    println(x<y)
+    println(x>y)
+    println(x<=y)
+    println(x>=y)
+    println(x==y)
+    println(x!=y)
 
 }
