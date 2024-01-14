6080a7c8ddf7d389d288d584fe6eef6f6f41891d
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-14 22:03:34 +0530

    Loops

---
+++ src/Practice/Loop.kt
@@ -0,0 +1,43 @@
+package Practice
+
+fun main(){
+    //for loop
+    //print 1 to 10 using for loop
+
+//    for (a in 1..10){
+//        println(a)
+//    }
+
+    //until
+//    for (i in 1 until  10){
+//        println(i)
+//    }
+
+    //reverse using for loop
+//    for (i in 10 downTo  1){
+//        println(i)
+//    }
+
+    //2 table using loop
+//    for (i in 2 .. 20 step 2){
+//        println(i)
+//    }
+
+
+    //while loop
+
+//    var a = 5
+//    while (a<=10){
+//        println("Sagar ")
+//        a++
+//    }
+
+    //do while
+    var a = 2
+    do {
+        println("*")
+        a++
+    }while (a<5)
+
+
+}
