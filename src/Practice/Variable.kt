9880f217cbfc6caa5803f1163b2d973a188288cc
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-14 21:21:06 +0530

    Variables

---
+++ src/Practice/Variable.kt
@@ -0,0 +1,21 @@
+package Practice
+
+fun main(){
+
+    /*Variables
+   * String => ""
+   * Int => -2147483648 to 2147483647
+   * Float => 1.4E-45 to 3.4028235E38
+   * Double => 4.9E-324 to 1.7976931348623157E308
+   * Char => 'A-Z' & 'a-z'
+   * Short => -32768 to 32767
+   * Long => -9223372036854775808 to 9223372036854775807
+   * Boolean => true & false
+   * Byte => -128 to 127*/
+
+    var getValues = Int.MIN_VALUE
+    val getmax = Int.MAX_VALUE
+    println(getValues)
+    println(getmax)
+
+}
