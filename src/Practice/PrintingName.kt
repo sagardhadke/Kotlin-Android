fb745731a0fd1574377a069ae52e11898bae9485
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-14 21:18:34 +0530

    Practice Form Start

---
+++ src/Practice/PrintingName.kt
@@ -0,0 +1,9 @@
+package Practice
+
+fun main(){
+    //printing personal information
+    val name = "Sagar Dhadke"
+    val age = 20 //in 2024
+    println("My name is $name")
+    println("I am $age is old")
+}
