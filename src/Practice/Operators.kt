e3231e53375be0179bd29c4c1b3ac722ebb7335a
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-14 22:06:18 +0530

    Arithmetical Operators

---
+++ src/Practice/Operators.kt
@@ -0,0 +1,22 @@
+package Practice
+
+fun main(){
+
+    //Arithmetic Operators
+
+    /*
+    * + addition
+    * - subtraction
+    * * multiplication
+    * / division
+    * % modules */
+
+    var a = 5
+    var b = 10
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+
+}
